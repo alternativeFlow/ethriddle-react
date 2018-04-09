78b71c78ab53d94ad0ab7ca92316d0599c72f38d
alternativeFlow <danxue@gmail.com>
2018-04-09 07:02:00 -0700

    mobile message

--- src/components/pages/pageComponents/HomepageHeaderBox.js
+++ src/components/pages/pageComponents/HomepageHeaderBox.js
@@ -7,8 +7,25 @@ import '../../../css/HomepageHeaderBox.css';
 
 class HomepageHeaderBox extends React.Component {
 
+	detectmobile = () => { 
+	 if( navigator.userAgent.match(/Android/i)
+	 || navigator.userAgent.match(/webOS/i)
+	 || navigator.userAgent.match(/iPhone/i)
+	 || navigator.userAgent.match(/iPad/i)
+	 || navigator.userAgent.match(/iPod/i)
+	 || navigator.userAgent.match(/BlackBerry/i)
+	 || navigator.userAgent.match(/Windows Phone/i)
+	 ){
+	    return true;
+	  }
+	 else {
+	    return false;
+	  }
+	}
+
 
 	render() {
+		let mobile = this.detectmobile();
 		return (
 			<div>
 				<Grid>
@@ -20,6 +37,12 @@ class HomepageHeaderBox extends React.Component {
 								{ this.props.renderMetamaskMessages ? (
 									<p>To get started, install Metamask. If you already have it installed, please sign in. If you need more info you can find it in the <Link to="/faq">FAQ.</Link></p>
 									) : (
+									<p></p>
+									)
+								}
+								{ mobile ? (
+									<p>Please note, this site is not optimized for mobile.</p>
+									): (
 									<p></p>
 									)
 								}
